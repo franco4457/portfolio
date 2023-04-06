3adc6dfb98cfa5d354aa798432e65c7647c2d8cc
franco4457 <franco_forex@hotmail.com>
2023-04-06 19:08:13 -0300

    colors from

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,13 +8,13 @@ export default function Input ({ handlerChange, value, type, name, labelText }) 
           value={value}
           type={type}
           name={name}
-          className="block py-2.5  w-full md:text-lg text-stone-900 bg-transparent border-0 border-b-2  border-stone-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          className="block py-2.5  w-full md:text-lg text-stone-900 bg-transparent border-0 border-b-2  border-stone-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-zinc-300 focus:outline-none focus:ring-0 focus:border-stone-600 peer"
           placeholder=" "
           required
         />
         <label
           htmlFor={name}
-          className="peer-focus:font-medium absolute md:text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          className="peer-focus:font-medium absolute md:text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-stone-600 peer-focus:dark:text-zinc-300 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           {labelText}
         </label>
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -57,8 +57,8 @@ export default function ContactMe () {
         />
         <div className="relative z-0 w-full mb-6 group">
           <textarea
-            className="block py-2.5 px-0 w-full md:text-lg text-stone-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+          className="block py-2.5  w-full md:text-lg text-stone-900 bg-transparent border-0 border-b-2  border-stone-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-zinc-300 focus:outline-none focus:ring-0 focus:border-stone-600 peer"
+          placeholder=" "
             // required
             onChange={handlerChange}
             value={inputs.message}
@@ -66,8 +66,8 @@ export default function ContactMe () {
           ></textarea>
           <label
             htmlFor="message"
-            className=" peer-focus:font-medium absolute md:text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
+            className="peer-focus:font-medium absolute md:text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-stone-600 peer-focus:dark:text-zinc-300 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
             Mensaje
           </label>
         </div>
