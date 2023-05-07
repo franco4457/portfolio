21852723fca7a995911fc1bbb7e7156a906a972b
franco4457 <franco_forex@hotmail.com>
2023-05-07 13:59:39 -0300

    update bait proyect

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,39 @@
 import { SiExpress } from 'react-icons/si'
 export const projects = [
   {
+    src: '/Bait.png',
+    link: 'https://bait-one.vercel.app/',
+    alt: 'BaitApp',
+    title: 'Bait',
+    text: (
+      <>
+        Trabajé en un equipo que desarrolló una aplicación Full Stack llamada
+        Bait, utilizando <strong>metodologías ágiles</strong>. La plataforma
+        permite a los usuarios crear reseñas de locales de comida y a los
+        administradores validar las reseñas, asignar roles y verificar los
+        locales. Mi papel en el proyecto incluyó la creación del back-end y la
+        comunicación con el front-end, así como tareas como la subida de
+        imágenes con <strong>Cloudinary</strong>, la autenticación de terceros
+        con <strong>Firebase</strong>, la implementación de{' '}
+        <strong>inteligencia artificial</strong> para analizar la toxicidad de
+        los comentarios, la documentación de endpoints con{' '}
+        <strong>Swagger</strong> y la creación de <strong>filtros y paginado</strong>.
+      </>
+    ),
+    techs: [
+      { src: '/react.png', name: 'React' },
+      { src: '/nodeJS.png', name: 'NodeJS' },
+      { src: '/postgreSQL.png', name: 'PostgreSQL' },
+      { src: '/redux.png', name: 'Redux' },
+      { src: '/cloudinary.png', name: 'Cloudinary' },
+      { src: '/firebase-logo.png', name: 'firebase-logo' },
+      { src: '/swagger-logo.png', name: 'swagger-logo' }
+    ],
+    icon: (
+      <SiExpress className="w-12 h-12 m-auto hover:scale-110 hover:rotate-12 transition" />
+    )
+  },
+  {
     src: '/ZooDogsAPP.png',
     link: 'https://pi-dogs-franco4457.vercel.app/',
     alt: 'AppZooDogs',
@@ -41,8 +74,9 @@ export const projects = [
         <strong> React, Redux y CSS puro</strong>, mientras que el backend se
         construyó con una
         <strong> REST API</strong> de <strong>Node.js</strong> creada con
-        <strong> Express</strong> y una base de datos <strong>PostgreSQL</strong>
-        . Aprendí a crear un <strong>registro de usuarios</strong>, implementar
+        <strong> Express</strong> y una base de datos{' '}
+        <strong>PostgreSQL</strong>. Aprendí a crear un{' '}
+        <strong>registro de usuarios</strong>, implementar
         <strong> autenticación</strong> con
         <strong> hash de contraseñas</strong> mediante la biblioteca bcrypt para
         garantizar la seguridad de los datos de los usuarios,
